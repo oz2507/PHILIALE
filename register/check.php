1d4a54e372f9a07f97e159aa1649fc9248f29f8b
Risakodama <moomin0175@gmail.com>
2018-06-02 13:11:06 +0800

    imgのpathを修正

--- register/check.php
+++ register/check.php
@@ -1,34 +1,36 @@
 <?php
-
   session_start();
+   // var_dump($_POST);
 
-  // SESSIONが得られていない場合signup画面に強制遷移
   if (!isset($_SESSION['register'])) {
     header("Location: signup.php");
     exit();
   }
 
-    // SESSION関数を使いやすく
+    // ①
     $name = $_SESSION['register']['name'];
     $email = $_SESSION['register']['email'];
     $user_password = $_SESSION['register']['password'];
+    // $img_name = $_SESSION['register']['img_name'];
+ 
+  // echo "<pre>";
+  // var_dump($_SESSION);
+  // echo "</pre>";
 
     if (!empty($_POST)) {
 
-        require('../dbconnect.php');
+      require('../dbconnect.php');
 
-        $sql = 'INSERT INTO `users` SET `name`=?, `email`=?, `password`=? ';
-        $data = array($name, $email, password_hash($user_password, PASSWORD_DEFAULT),);
-        $stmt = $dbh->prepare($sql);
-        $stmt->execute($data);
+          $sql = 'INSERT INTO `users` SET `name`=?, `email`=?, `password`=? ';
+          $data = array($name, $email, password_hash($user_password, PASSWORD_DEFAULT),);
+          $stmt = $dbh->prepare($sql);
+          $stmt->execute($data);
 
-        $dbh = null;
+          $dbh = null;
 
-        // ログインしている訳でなはいのでセッションを破棄
-        unset($_SESSION['register']);
-
-        header('Location: thanks.html');
-        exit();
+      unset($_SESSION['register']);
+          header('Location: thanks.html');
+          exit();
 
     }
 
@@ -38,10 +40,10 @@
 <html lang="ja">
 <head>
   <meta charset="utf-8">
-  <title>PHILIALE</title>
+  <title>Learn SNS</title>
   <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
   <link rel="stylesheet" type="text/css" href="../assets/css/style_r.css">
-  
+  <!-- <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css"> -->
 </head>
 <body style="margin: 60px 0;">
   <div class="container">
@@ -51,36 +53,35 @@
         <br><br><br>
         <div class="row">
           <div class="col-xs-12">
-
             <div class="thumbnail">
               <span>ユーザーネーム</span>
               <p class="lead text-center"><?php echo htmlspecialchars($name); ?>&nbsp;様</p>
             </div>
-
             <div class="thumbnail">
               <span>メールアドレス</span>
               <p class="lead text-center"><?php echo htmlspecialchars($email); ?></p>
             </div>
-
             <div class="thumbnail">
-              <span>パスワード</span>  
+              <span>パスワード</span>
+              <!-- ② -->
               <p class="lead text-center">●●●●●●●●</p>
             </div>
-            
+            <!-- ③ -->
             <form method="POST" action="">
-              
+              <!-- ⑤ -->
               <input type="hidden" name="action" value="submit">
               <input type="submit" class="btn btn-secondary btn-lg btn-block" value="  ユーザー登録  ">
-              
+              <!-- ④ -->
               <a href="signup.php" class="btn btn-default btn-lg btn-block">&laquo;&nbsp;戻る</a>
-
             </form>
-
           </div>
         </div>
       </div>
     </div>
   </div>
-
+  <!-- <script src="../assets/js/jquery-3.1.1.js"></script> -->
+  <!-- <script src="../assets/js/jquery-migrate-1.4.1.js"></script> -->
+  <!-- <script src="../assets/js/bootstrap.js"></script> -->
 </body>
 </html>
+
--- register/signup.php
+++ register/signup.php
@@ -1,82 +1,123 @@
 <?php
-
-  session_start();
-
-  $errors = array();
-
-  if (!empty($_POST)) { 
-      // ポスト送信があったとき以下を実行
-      $name = $_POST['input_name'];
-      $email = $_POST['input_email'];
-      $password = $_POST['input_password'];
-      // passwordの文字列を数える
-      $count = strlen($password);
+    // PHPプログラム
+session_start();
 
 
-        // ユーザー名の空チェック
-        if ($name == '') {
+$errors = array();
+
+    if (!empty($_POST)) { //ポスト送信があったとき以下を実行
+        $name = $_POST['input_name'];
+        $email = $_POST['input_email'];
+        $password = $_POST['input_password'];
+
+        $count = strlen($password);// hogehogeとパスワードを入力した場合、8が$countに代入される
+
+
+           // ユーザー名の空チェック
+          if ($name == '') {
             $errors['name'] = 'blank';
-        }else{
-            require('../dbconnect.php');
+          }
 
-            $sql = 'SELECT COUNT(*) as `cnt` FROM `users` WHERE `name`=?';
-            $data = array($name);
-            $stmt = $dbh->prepare($sql);
-            $stmt->execute($data);
+          else{
+                require('../dbconnect.php');
 
-            $dbh = null;
-            $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+                $sql = 'SELECT COUNT(*) as `cnt` FROM `users` WHERE `name`=?';
+                $data = array($name);
+                $stmt = $dbh->prepare($sql);
+                $stmt->execute($data);
 
-            if ($rec['cnt'] > 0) {
-            //数が0以上ですでに登録済み
-                $errors['name'] = 'duplication';
-            }
-        }
+                $dbh = null;
 
-        // メールアドレスの空チェック
-        if ($email == '') {
+                $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                // var_dump($rec);
+
+                if ($rec['cnt'] > 0) {//メールアドレスの数が0異常ですでに登録済み
+                  $errors['name'] = 'duplication';
+                }
+
+          }
+
+          if ($email == '') {
             $errors['email'] = 'blank';
-        }else{
-            require('../dbconnect.php');
+          }
+          //重複しているかの確認
+          else{
+                require('../dbconnect.php');
 
-            $sql = 'SELECT COUNT(*) as `cnt` FROM `users` WHERE `email`=?';
-            $data = array($email);
-            $stmt = $dbh->prepare($sql);
-            $stmt->execute($data);
+                $sql = 'SELECT COUNT(*) as `cnt` FROM `users` WHERE `email`=?';
+                $data = array($email);
+                $stmt = $dbh->prepare($sql);
+                $stmt->execute($data);
 
-            $dbh = null;
-            $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+                $dbh = null;
 
-            if ($rec['cnt'] > 0) {
-            //メールアドレスの数が0以上ですでに登録済み
-                $errors['email'] = 'duplication';
-            }
-        }
+                $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                // var_dump($rec);
+
+                if ($rec['cnt'] > 0) {//メールアドレスの数が0異常ですでに登録済み
+                  $errors['email'] = 'duplication';
+                }
+
+          }
 
 
-        if ($password == '') {
+          if ($password == '') {
             $errors['password'] = 'blank';
-        }elseif ($count < 4 || $count > 16) {
+          }
+
+          elseif ($count < 4 || $count > 16) {
             $errors['password'] = 'length';
-        }
+          }
 
-            if (empty($errors)) {
-                $_SESSION['register']['name'] = $_POST['input_name'];
-                $_SESSION['register']['email'] = $_POST['input_email'];
-                $_SESSION['register']['password'] = $_POST['input_password'];
-                // 上記3つは$_SESSION['register'] = $_POST;
+        // //画像名を取得
+        //   $file_name = $_FILES['input_img_name']['name'];
+        // // echo $file_name."<br>";
+        //   if (!empty($file_name)) {
+        //     $file_type = substr($file_name, -3); // 画像名の後ろから3文字を取得
+        //       $file_type = strtolower($file_type); // 大文字が含まれていた場合すべて小文字化
+        //       if ($file_type != 'jpg' && $file_type != 'png' && $file_type != 'gif') {
+        //         $errors['img_name'] = 'type';
+        //       }
+        //   }
+            
+        //   else {
+        //   $errors['img_name'] = 'blank';
+        //   }
 
-                header('Location: check.php');
-                exit();
-            }
-  }
+          if (empty($errors)) {
+            $date_str = date('YmdHis'); // YmdHisを指定することで取得フォーマットを指定
+            $submit_file_name = $date_str . $file_name;
+            // echo $date_str;
+            // echo "<br>";
+            // echo $submit_file_name;
+            move_uploaded_file($_FILES['input_img_name']['tmp_name'], '../user_profile_img/'.$submit_file_name);
+
+            $_SESSION['register']['name'] = $_POST['input_name'];
+            $_SESSION['register']['email'] = $_POST['input_email'];
+            $_SESSION['register']['password'] = $_POST['input_password'];
+            // 上記3つは$_SESSION['register'] = $_POST;という書き方で1文にまとめることもできます
+            $_SESSION['register']['img_name'] = $submit_file_name;
+
+
+            header('Location: check.php');
+            exit();
+
+          }
+        // echo "<pre>";
+        // var_dump($file_type);
+        // var_dump($errors);
+        // echo "</pre>";
+
+    }
     
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="utf-8">
-  <title>PHILIALE</title>
+  <title>Learn SNS</title>
   <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
   <link rel="stylesheet" type="text/css" href="../assets/css/style_r.css"> <!-- 追加 -->
   <!-- <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css"> -->
@@ -88,17 +129,17 @@
       <div class="col-xs-12 col-md-6 left-column">
         <h3 class="content_header">アカウント新規登録</h3>
           <p><label>PHILIALEのご利用有難うございます。<br>ご入館の前に、ご登録をお願いいたします。<br></label></p>
-          <img class="img-responsive img-circle logo_original" src="../assets/img/cat_collection/フィリアさん1.png">
+          <img class="img-responsive img-circle logo_original" src="../assets/img/フィリアさん1.png">
           <p>当館では文庫やコミック、情報誌などなんでもご登録出来ます。<br>本好きな当館支配人もまた、数多の本をお持ちだそうです。<br>皆様はどのような本がお好きでしょう。<br></p><p><br></p>
 
       </div>
 
       <!-- ここに右サイドコンテンツ -->
       <div class="col-xs-12 col-md-6 right-column">
-        <!-- アカウント作成 -->
+        <!-- <h2 class="text-center content_header">アカウント作成</h2> -->
         <form method="POST" action="signup.php" enctype="multipart/form-data">
           <div class="form-group">
-            <!-- ユーザー名の入力欄 -->
+            <!-- <label for="name">ユーザー名</label> -->
             <input type="text" name="input_name" class="form-control" id="name" placeholder="ユーザーネーム" style="height:3em;">
               <?php if(isset($errors['name']) && $errors['name'] == 'blank') { ?>
               <p class="text-danger">ユーザー名を入力してください</p>
@@ -109,7 +150,7 @@
           </div>
 
           <div class="form-group">
-            <!-- メールアドレスの入力欄 -->
+            <!-- <label for="email">メールアドレス</label> -->
             <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com">
             <?php if(isset($errors['email']) && $errors['email'] == 'blank') { ?>
               <p class="text-danger">メールアドレスを入力してください</p>
@@ -120,7 +161,7 @@
           </div>
 
           <div class="form-group">
-            <!-- パスワード入力欄 -->
+            <!-- <label for="password">パスワード</label> -->
             <input type="password" name="input_password" class="form-control" id="password" placeholder="パスワード">
             <?php if(isset($errors['password']) && $errors['password'] == 'blank') { ?>
               <p class="text-danger">パスワードを入力してください</p>
@@ -131,7 +172,7 @@
           </div>
 
           <div class="form-group">
-            <!-- 確認用パスワード -->
+            <!-- <label for="password">確認用パスワード</label> -->
             <input type="password" name="input_chk_password" class="form-control" id="chk_password" placeholder="確認用パスワード">
             <?php if(isset($errors['chk_password']) && $errors['chk_password'] == 'blank') { ?>
               <p class="text-danger">確認用パスワードを入力してください</p>
@@ -140,7 +181,6 @@
               <p class="text-danger">パスワードは4〜16文字で入力してください</p>
             <?php } ?>
           </div>
-          
           <br><br><br>
           <input type="submit" class="btn btn-secondary btn-lg btn-block" value="登録情報の確認">
           <a href="../signin.php" style="float: right; padding-top: 6px;" class="text-success">サインイン</a>
@@ -149,6 +189,8 @@
 
     </div>
   </div>
-
+  <!-- <script src="../assets/js/jquery-3.1.1.js"></script> -->
+  <!-- <script src="../assets/js/jquery-migrate-1.4.1.js"></script> -->
+  <!-- <script src="../assets/js/bootstrap.js"></script> -->
 </body>
 </html>
