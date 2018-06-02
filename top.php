e0084e4b75b778f4328bb46bf023d4275e80c0d7
exchange-wata <36654133+exchange-wata@users.noreply.github.com>
2018-06-02 11:13:55 +0800

    ブランチ間違えたので一度コミットします

--- top.php
+++ top.php
@@ -3,10 +3,23 @@
 <head>
 	<meta charset="UTF-8">
 	<title>PHILIALE</title>
+
+	<link rel="stylesheet" href="assets/css/index.css">
 </head>
 <body>
 <!-- navbar -->
-	
-
+<nav class="navbar navbar-default navbar-fixed-top">
+	<div class="navbar-header">
+		<div>
+			<!-- left icon -->
+			<!-- right menu -->
+			<ul>
+				<li></li>
+				<li></li>
+			</ul>
+		</div>
+		
+	</div>
+</nav>
 </body>
 </html>
