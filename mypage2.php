c5bb5c803c3167ed0889af5da79a4acdeacdba78
exchange-wata <36654133+exchange-wata@users.noreply.github.com>
2018-06-03 17:11:25 +0800

    マイページ

--- mypage2.php
+++ mypage2.php
@@ -5,12 +5,114 @@
 	<title>MYPAGE</title>
 
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-	<link rel="stylesheet" href="assets/css/top.css">
+	<link rel="stylesheet" href="assets/css/mypage.css">
+
 
 
 	
 </head>
 <body>
+<!-- navbar -->
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarEexample">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button> 
+
+            <a class="navbar-brand-center" style="position: absolute;top:10px;z-index:2000;"  href="#">
+                <img alt="philia" src="assets/img/philia2.png" style="height: 35px;">
+            </a>       
+        
+        </div>
+
+        <div class="collapse navbar-collapse" id="navbarEexample">
+            <ul class="nav navbar-nav">
+            	<li><a href="#">読みたい</a></li>
+            	<li class="right diviver">|</li>
+                <li><a href="#">読んだ</a></li>
+            </ul>
+
+          	<form class="navbar-form navbar-right" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="文庫検索">
+                </div>
+                <button type="submit" class="btn btn-default">検索</button>
+            </form>
+
+  		</div>
+    </div>
+</nav>
+
+<!-- add book -->
+<div class="row">
+	<div class="col-xs-6 col-md-3">
+		<div class="thumbnail">
+			<img src="assets/img/add1.png">
+			<div class="caption">
+				<h3>サムネイル・ラベル</h3>
+				<p>段落。</p>
+				<p>...</p>
+				<p><a href="#" class="btn btn-default" role="button" target="_blank">ボタン</a></p>
+			</div>
+		</div>
+	</div>
+
+
+
+
+<!-- book -->
+	<div class="col-xs-6 col-md-3">
+		<div class="thumbnail">
+			<img src="assets/img/philia2.png">
+			<div class="caption">
+				<h3>サムネイル・ラベル</h3>
+				<p>段落。</p>
+				<p>...</p>
+				<p><a href="#" class="btn btn-default" role="button" target="_blank">ボタン</a></p>
+			</div>
+		</div>
+	</div>
+	<div class="col-xs-6 col-md-3">
+		<div class="thumbnail">
+			<img src="assets/img/philia2.png">
+			<div class="caption">
+				<h3>サムネイル・ラベル</h3>
+				<p>段落。</p>
+				<p>...</p>
+				<p><a href="#" class="btn btn-default" role="button" target="_blank">ボタン</a></p>
+			</div>
+		</div>
+	</div>
+	<div class="col-xs-6 col-md-3">
+		<div class="thumbnail">
+			<img src="assets/img/philia2.png">
+			<div class="caption">
+				<h3>サムネイル・ラベル</h3>
+				<p>段落。</p>
+				<p>...</p>
+				<p><a href="#" class="btn btn-default" role="button" target="_blank">ボタン</a></p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="row">
+<div class="col-xs-6 col-md-3">
+		<div class="thumbnail">
+			<img src="assets/img/philia2.png">
+			<div class="caption">
+				<h3>サムネイル・ラベル</h3>
+				<p>段落。</p>
+				<p>...</p>
+				<p><a href="#" class="btn btn-default" role="button" target="_blank">ボタン</a></p>
+			</div>
+		</div>
+	</div>
+</div>
 
 
 <!-- 5.footer -->
