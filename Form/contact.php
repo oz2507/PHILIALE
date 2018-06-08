274bf00016beb5af98a275d4c8030297335660c8
Risakodama <moomin0175@gmail.com>
2018-06-08 11:25:21 +0800

    タイトルLearnSNSからPHILIALEに修正

--- Form/contact.php
+++ Form/contact.php
@@ -47,7 +47,7 @@
 <html lang="ja">
 <head>
   <meta charset="utf-8">
-  <title>Learn SNS</title>
+  <title>PHILIALE</title>
   <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
   <link rel="stylesheet" type="text/css" href="../assets/css/style_r.css">
   <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css">
