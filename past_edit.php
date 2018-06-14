ad0156e0dfd908b65c09442f215ada2ae19cd1ee
exchange-wata <36654133+exchange-wata@users.noreply.github.com>
2018-06-14 20:03:42 +0800

    綴り修正

--- past_edit.php
+++ past_edit.php
@@ -6,13 +6,15 @@
 
 	$book_title=$_POST['book_title'];
 	$book_author=$_POST['book_author'];
-	$book_img=$_POST["book_img"];
+	// $book_img=$_POST["book_img"];
 	$comment=$_POST["comment"];
 
 
-	$sql='UPDATE `past_archives` SET `book_title`=?, `book_author`=?, `book_img`=?, `comment`=? WHERE `id`=?';
+	$sql='UPDATE `past_archives` SET `book_title`=?, `book_author`=?, `comment`=? WHERE `id`=?';
 
-	$data=array($book_title,$book_author,$book_img,$comment,$book_id);
+	$data=array($book_title,$book_author,$comment,$book_id);
 	$stmt = $dbh->prepare($sql);
     $stmt->execute($data);
+
+    header("Location:mypage2.php");
 ?>
