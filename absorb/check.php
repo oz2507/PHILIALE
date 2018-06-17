9aa110c161220afd39a3a085b0d43a50a95ed726
oz2507 <oz.nytia07@gmai.com>
2018-06-17 21:17:11 +0800

    スプレッドシート読み取りの場合分けを記載

--- absorb/check.php
+++ absorb/check.php
@@ -1,13 +1,14 @@
 <?php
 
-$id = $_GET['id'];
+    $id = $_GET['id'];
 
-$data = "https://spreadsheets.google.com/feeds/list/$id/od6/public/values?alt=json";
+    $data = "https://spreadsheets.google.com/feeds/list/$id/od6/public/values?alt=json";
 
-$json = file_get_contents($data);
-$json_decode = json_decode($json);
 
-$books = $json_decode->feed->entry;
+    $json = file_get_contents($data);
+    $json_decode = json_decode($json);
+
+    $books = $json_decode->feed->entry;
 
 ?>
 
@@ -20,18 +21,22 @@ $books = $json_decode->feed->entry;
 
   <h1>検索結果</h1>
   
-  <?php if (isset($_GET['id'])) { 
+  <?php if (!empty($books)) { 
    foreach($books as $book ){?>
   <p>本のタイトル :<?php echo $book->title->{'$t'}; ?></p>
 
   <p><?php echo $book->content->{'$t'}; ?></p>
   <p>------------------------------------------------</p>
   <?php } ?>
+  <?php }else{
+    echo "IDが正しくない可能性があります。"; ?>
+    <a href="submit.php">戻る</a>
   <?php } ?>
-
+  <?php if (!empty($books)) { ?>
   <a href="absorb.php?id=<?php echo $id; ?>">
   	<p>保管</p> 
   </a>
+  <?php } ?>
   
 </body>
 </html>
