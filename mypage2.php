6efb95d4cb51afd49528e2420cb09aeaa1fa4d10
石野太一 <>
2018-06-19 12:10:01 +0800

    mypageに「scriptでpast_detail」を呼び出すのがなかったので追加

--- mypage2.php
+++ mypage2.php
@@ -214,7 +214,7 @@
 
 <script src="assets/js/jquery-3.1.1.js"></script>
 <script src="detail_pop/past_detail.js"></script>
-<script src="detail_pop/past_detail.js"></script>
+<script src="detail_pop/future_detail.js"></script>
 
 <script src="assets/js/jquery-migrate-1.4.1.js"></script>
 <script src="assets/js/bootstrap.js"></script>
