b9cd1f993d5c921a021d8baf7dc89fec9bb13971
oz2507 <oz.nytia07@gmai.com>
2018-10-10 19:25:34 +0900

    no message

--- absorb/check.php
+++ absorb/check.php
@@ -4,7 +4,7 @@ function getApiData($url){
     $options = [
         'http' => [
             'method'  => 'GET',
-            'timeout' => 10, // タイムアウト時間
+            'timeout' => 10, 
         ]
     ];
 }
@@ -47,47 +47,47 @@ if (isset($_GET['id'])) {
     <?php require("../partial/favicon.php"); ?>
   </head>
   <body>
-  <!-- navbar -->
-  <div class="nav_container navbar-fixed-top">
-    <div class="nav_header">
-      <img alt="philia" src="../assets/img/philia2.png" style="height: 35px;">
-    </div>
-  </div>
-
-  <!-- main -->
-  <div class="container">
-    <!-- import button -->
-    <div class="row">
-      <div class="head">
-        <h1>検索結果</h1>
-
-        <?php if (!empty($books)) { ?>
-          <a href="absorb.php?id=<?php echo $id; ?>">
-            <button class="import_btn">この内容でフィリアールの書庫に保管</button>
-          </a>
-        <?php } ?>
+    <!-- navbar -->
+    <div class="nav_container navbar-fixed-top">
+      <div class="nav_header">
+        <img alt="philia" src="../assets/img/philia2.png" style="height: 35px;">
       </div>
     </div>
 
-    <!-- <h1>検索結果</h1> -->
-    <?php if ($json_decode !== null) { 
-        foreach($books as $book){ ?>
+    <!-- main -->
+    <div class="container">
+      <!-- import button -->
       <div class="row">
-        <div class="col-xs-10 col-xs-offset-1 col-md-8 col-md-offset-2">
-          <div class="list_form">
-            <p>作品名 :<span><?php echo $book->title->{'$t'}; ?>&ensp;</span></p>
-            <p>著者名 :<span><?php echo $book->{'gsx$作者'}->{'$t'}; ?></span></p>
-          </div>
+        <div class="head">
+          <h1>検索結果</h1>
+
+          <?php if (!empty($books)) { ?>
+            <a href="absorb.php?id=<?php echo $id; ?>">
+              <button class="import_btn">この内容でフィリアールの書庫に保管</button>
+            </a>
+          <?php } ?>
         </div>
       </div>
-    <?php } ?>
-    <?php } else {
-      $message = "IDが正しくない可能性があります。"; ?>
-      <div style="text-align: center;">
-        <p><?php echo $message; ?></p>
-        <a href="submit_2.php"><button class="import_btn">戻る</button></a>
-      </div>
-    <?php } ?>
+
+      <!-- <h1>検索結果</h1> -->
+      <?php if ($json_decode !== null) { 
+          foreach($books as $book){ ?>
+        <div class="row">
+          <div class="col-xs-10 col-xs-offset-1 col-md-8 col-md-offset-2">
+            <div class="list_form">
+              <p>作品名 :<span><?php echo $book->title->{'$t'}; ?>&ensp;</span></p>
+              <p>著者名 :<span><?php echo $book->{'gsx$作者'}->{'$t'}; ?></span></p>
+            </div>
+          </div>
+        </div>
+      <?php } ?>
+      <?php } else {
+        $message = "IDが正しくない可能性があります。"; ?>
+        <div style="text-align: center;">
+          <p><?php echo $message; ?></p>
+          <a href="submit_2.php"><button class="import_btn">戻る</button></a>
+        </div>
+      <?php } ?>
 
     </div><!-- container -->
 
